6bf89e88c5ccf67138d23a844f6ec3d27abcb90e
Claire Gammon <claire.gammon@gmail.com>
2018-11-13 22:40:35 -0700

    scaffold

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { SearchComponent } from './search/search.component';
+import { SubmitReviewComponent } from './submit-review/submit-review.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+    // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  },
+  {
+    path: 'book-details',
+    component: BookDetailsComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  },
+  {
+    path: 'submit-review',
+    component: SubmitReviewComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { SearchComponent } from './search/search.component';
+import { SubmitReviewComponent } from './submit-review/submit-review.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookDetailsComponent,
+    SearchComponent,
+    SubmitReviewComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
